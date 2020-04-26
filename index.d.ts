4f2100c96b4bb0521cec27a2a2b17cf76f43eea9
EXtremeExploit <gdextremeexploit@gmail.com>
2020-04-26 00:09:31 -0300

    Fixed ts false error

--- index.d.ts
+++ index.d.ts
@@ -7,7 +7,7 @@ export declare class LevelCalculator {
          * @param {number} currentScore Total score of the user.
          * @returns {number}  If `currentScore` is 0 then will return total score needed, else just the amount needed for that level
          */
-    public ScoreLevelCalculator(level: number, currentScore: number = 0): number;
+    public ScoreLevelCalculator(level: number, currentScore?: number): number;
 
     private bcpow(base: number, exp: number, scale: number);
 }
