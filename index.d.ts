bf5f33c26438092a1de92a831f4b0c48f09b9c0f
EXtremeExploit <gdextremeexploit@gmail.com>
2021-04-01 20:42:09 -0300

    Use simpler calling

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,7 @@
-export declare class LevelCalculator {
-    constructor();
-
-    /**
-         * Calculates The Total score needed for a certain level
-         * @param {number} level Level to target
-         * @param {number} currentScore Total score of the user.
-         * @returns {number}  If `currentScore` is 0 then will return total score needed, else just the amount needed for that level
-         */
-    public ScoreLevelCalculator(level: number, currentScore?: number): number;
-
-    private bcpow(base: number, exp: number, scale: number);
-}
-
-module.exports = typeof LevelCalculator;
+/**
+ * Calculates The Total score needed for a certain level
+ * @param {number} level Level to target
+ * @param {number} currentScore Total score of the user.
+ * @returns {number}  If `currentScore` is 0 then will return total score needed, else just the amount needed for that level
+ */
+export default function ScoreLevelCalculator(level: number, currentScore?: number): number;
